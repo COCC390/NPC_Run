bd424119387957d5b4f4fa08c6ae85564bced7da
Nguyễn Văn Khánh <57382524+COCC390@users.noreply.github.com>
2023-08-04 21:51:05 +0700

    small update for jumping

--- AI_NPC_Run/Assets/Scripts/NPCControl/JumpTask.cs
+++ AI_NPC_Run/Assets/Scripts/NPCControl/JumpTask.cs
@@ -3,29 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using QRun.BehaviorTree;
 
-public class JumpTask : Node
-{
+public class JumpTask : Node {
     private NPCController _npcController;
     private Transform _npcTransform;
     private Rigidbody _npcRigidbody;
     private NPCSensor _npcSensorController;
 
-    public JumpTask(NPCController npcController, Transform npcTransform, Rigidbody npcRigidBody, NPCSensor npcSensorController)
-    { 
+    public JumpTask(NPCController npcController, Transform npcTransform, Rigidbody npcRigidBody, NPCSensor npcSensorController) {
         _npcController = npcController;
         _npcTransform = npcTransform;
         _npcRigidbody = npcRigidBody;
         _npcSensorController = npcSensorController;
     }
 
-    public override NodeState Evaluate()
-    {
-        if(_npcSensorController.stuckByObstacleType == ObstacleType.JumpableObstacle)
-        {
+    public override NodeState Evaluate() {
+        if (_npcSensorController.stuckByObstacleType == ObstacleType.JumpableObstacle) {
             // Do Jump
-            Debug.Log("Do jump " + CalculateVelocity());
+            Debug.Log("Do jump ");
+            _npcTransform.LookAt(_npcSensorController.obstacle.transform);
 
+            float jumpVelocity = Mathf.Sqrt(2 * 9.8f * _npcController.JumpHeight);
+            Vector3 jumpDirection = (_npcSensorController.obstacle.transform.position - _npcTransform.position).normalized;
 
+            _npcRigidbody.velocity = new Vector3(jumpDirection.x * 20f, jumpVelocity, jumpDirection.z * 20f);
 
             state = NodeState.Success;
             return state;
@@ -35,15 +35,4 @@ public class JumpTask : Node
         return state;
     }
 
-    private void DoJump()
-    {
-        //_npcRigidbody.velocity = CalculateVelocity();
-    }
-
-    private float CalculateVelocity()
-    {
-        float A = (_npcController.JumpHeight + _npcTransform.position.magnitude + Mathf.Tan(_npcController.JumpAngle) - (9.8f * _npcTransform.position.sqrMagnitude)) / _npcSensorController.obstacle.transform.position.magnitude;
-        float B = A / Mathf.Cos(_npcController.JumpAngle);
-        return Mathf.Sqrt(B / 2);
-    }
 }
